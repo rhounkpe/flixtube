0741532ac850850705a9043f8d8ea06f6535c978
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2021-04-26 19:45:25 +0200

    Do not include videos in git

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 'use strict';
 const express = require('express');
+const fs = require('fs');
 const app = express();
-const port = 3000;
+
+if(!process.env.FLIXTUBE_PORT) {
+    throw new Error('Please, specify the port number for the HTTP server with the environmrnt variable FLIXTUBE_PORT');
+}
+
+const port = process.env.FLIXTUBE_PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
+});
+
+app.get('/video', (req, res) => {
+    const path = './videos/20210416_130250.mp4';
+
+    fs.stat(path, (err, stats) => {
+        if(err) {
+            console.error('An error occured');
+            res.sendStatus(500);
+            return;
+        }
+
+        res.writeHead(200, {
+            'Content-Length': stats.size,
+            'Content-Type': 'video/mp4',
+        });
+
+        fs.createReadStream(path).pipe(res);
+    });
+
 });
 
 app.listen(port, () => {
